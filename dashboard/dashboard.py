be8c3981232fe284396e495619025b78f5ab5440
acaciel <129807116+acaciel@users.noreply.github.com>
2025-03-14 01:50:28 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -11,18 +11,22 @@ sns.set(style='whitegrid')
 def create_weather_rent_df(df):
     weather_rent_df = df.groupby(by='weather_situation').agg({
         'count': 'sum'
-    })
+    }).reset_index()
+
+    weather_rent_df['count'] = weather_rent_df['count'].astype(int)
     return weather_rent_df
     
 # Menyiapkan season_rent_df
 def create_season_rent_df(df):
-    season_rent_df = df.groupby(by='season')[['registered', 'casual']].sum().reset_index()
+    season_rent_df = df.groupby(by='season').agg({
+        'count': 'sum'
+    })
     return season_rent_df
 
 # Menyiapkan weekday_rent_df
 def create_weekday_rent_df(df):
     weekday_rent_df = df.groupby(by='weekday').agg({
-        'count': 'sum'
+        'count':'sum'
     }).reset_index()
     return weekday_rent_df
 
@@ -30,18 +34,20 @@ def create_weekday_rent_df(df):
 def create_workingday_rent_df(df):
     workingday_rent_df = df.groupby(by='workingday').agg({
         'count': 'sum'
-    }).reset_index()
+    })
     return workingday_rent_df
 
 # Menyiapkan holiday_rent_df
 def create_holiday_rent_df(df):
     holiday_rent_df = df.groupby(by='holiday').agg({
         'count': 'sum'
-    }).reset_index()
+    })
     return holiday_rent_df
 
 # Load cleaned data
 day_df = pd.read_csv("dashboard/day_data.csv")
+day_df['date'] = pd.to_datetime(day_df['date'])
+
 
 # Membuat komponen filter
 min_date = day_df['date'].min()
@@ -77,39 +83,33 @@ nude_palette = ["#F4E1D2", "#E6B8A2", "#D99B84", "#C2856D", "#A86A50"]
 
 fig, ax = plt.subplots(figsize=(12, 6))
 
-sns.barplot(x=weather_rent_df.index, y=weather_rent_df['count'], palette=nude_palette, ax=ax)
-
-ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: f'{int(x):,}'))
-
-for index, row in enumerate(weather_rent_df['count']):
-    ax.text(index, row + 1, str(row), ha='center', va='bottom', fontsize=12)
+sns.barplot(x='weather_situation', y='count', data=weather_rent_df, palette=nude_palette, ax=ax)
 
 ax.set_xlabel(None)
 ax.set_ylabel(None)
 ax.tick_params(axis='x', labelsize=15)
 ax.tick_params(axis='y', labelsize=12)
+
+ax.yaxis.set_major_locator(ticker.MaxNLocator(integer=True))
+ax.get_yaxis().get_major_formatter().set_scientific(False)
 st.pyplot(fig)
 
 # Season Rentals
 st.subheader('Seasonly Rentals')
 
 # Menyiapkan total penyewaan per musim
-season_rent_df['total'] = season_rent_df['registered'] + season_rent_df['casual']
 
 fig, ax = plt.subplots(figsize=(12, 6))
 
-sns.barplot(x='season', y='total', data=season_rent_df, palette=nude_palette, ax=ax)
-
-ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: f'{int(x):,}'))
-
-# Menambahkan label angka di atas batang
-for index, row in enumerate(season_rent_df['total']):
-    ax.text(index, row + 5000, str(row), ha='center', va='bottom', fontsize=12)
+sns.barplot(x='season', y='count', data=season_rent_df, palette=nude_palette, ax=ax)
 
 ax.set_xlabel(None)
 ax.set_ylabel(None)
 ax.tick_params(axis='x', labelsize=14)
 ax.tick_params(axis='y', labelsize=12)
+
+ax.yaxis.set_major_locator(ticker.MaxNLocator(integer=True))
+ax.get_yaxis().get_major_formatter().set_scientific(False)
 st.pyplot(fig)
 
 # Membuat jumlah penyewaan berdasarkan weekday, working dan holiday
@@ -119,9 +119,6 @@ fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(14,12))
 # Berdasarkan weekday
 sns.barplot(x='weekday', y='count', data=weekday_rent_df, palette=nude_palette, ax=axes[0])
 
-for index, row in enumerate(weekday_rent_df['count']):
-    axes[0].text(index, row + 1, str(row), ha='center', va='bottom', fontsize=12)
-
 axes[0].set_title('Number of Rents based on Weekday')
 axes[0].set_ylabel(None)
 axes[0].tick_params(axis='x', labelsize=14)
@@ -130,27 +127,22 @@ axes[0].tick_params(axis='y', labelsize=12)
 # Berdasarkan workingday
 sns.barplot(x='workingday', y='count', data=workingday_rent_df, palette=nude_palette[:2], ax=axes[1])
 
-for index, row in enumerate(workingday_rent_df['count']):
-    axes[1].text(index, row + 1, str(row), ha='center', va='bottom', fontsize=12)
-
 axes[1].set_title('Number of Rents based on Working Day')
 axes[1].set_ylabel(None)
 axes[1].tick_params(axis='x', labelsize=14)
 axes[1].tick_params(axis='y', labelsize=12)
+axes[1].yaxis.set_major_locator(ticker.MaxNLocator(integer=True))
+axes[1].get_yaxis().get_major_formatter().set_scientific(False)
 
 # Berdasarkan holiday
 sns.barplot(x='holiday', y='count', data=holiday_rent_df, palette=nude_palette[:2], ax=axes[2])
-
-for index, row in enumerate(holiday_rent_df['count']):
-    axes[2].text(index, row + 1, str(row), ha='center', va='bottom', fontsize=12)
 
 axes[2].set_title('Number of Rents based on Holiday')
 axes[2].set_ylabel(None)
 axes[2].tick_params(axis='x', labelsize=14)
 axes[2].tick_params(axis='y', labelsize=12)
-
-for ax in axes:
-    ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: f'{int(x):,}'))
+axes[2].yaxis.set_major_locator(ticker.MaxNLocator(integer=True))
+axes[2].get_yaxis().get_major_formatter().set_scientific(False)
 
 plt.tight_layout()
 st.pyplot(fig)
